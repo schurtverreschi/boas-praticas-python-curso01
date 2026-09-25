b2c40bed558da72bec634a5a757176dbb3b8ef81
Schurt Verreschi <schurtverreschi@gmail.com>
2026-09-25 14:27:53 -0300

    refactor: corrige nomenclaturas conforme PEP 8
    Altera o nome de variaveis e metodos de acrodo com a PEP 8 para deixar o código mais legivél

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from fastapi import FastAPI
 from fastapi import APIRouter, HTTPException
 
 
-
-
 # Modelo base para produto
 class ProdutoBase(BaseModel):
     nome: str
@@ -16,6 +14,7 @@ class ProdutoBase(BaseModel):
 # Modelo para criar um produto
 class CriarProduto(ProdutoBase):
     pass
+
 
 # Modelo de produto com ID
 class Produto(ProdutoBase):
@@ -39,88 +38,96 @@ class Usuario(BaseModel):
     nome: str
 
 
-Produtos                 =[]
-ContadorProduto          =1
+produtos = []
+contador_produto = 1
+
+usuarios = []
+contador_usuario = 1
 
 
-Usuarios                 =[]
-
-ContadorUsuario          =1
-
-
-ConstanteMensagemHome    ="Bem-vindo à API de Recomendação de Produtos"
+MENSAGEM_HOME = "Bem-vindo à API de Recomendação de Produtos"
 
 # Histórico de compras em memória
-HistoricoDeCompras         ={}
+historico_de_compras = {}
 
 # Criando o App
 app = FastAPI()
 
-# Iniciando o servidor
 
+# Iniciando o servidor
 @app.get("/")
 def home():
-    global ConstanteMensagemHome
-    return {"mensagem": ConstanteMensagemHome}
+    global MENSAGEM_HOME
+    return {"mensagem": MENSAGEM_HOME}
 
 
 # Rota para cadastrar produtos
-
 @app.post("/produtos/", response_model=Produto)
-def criarproduto(produto: CriarProduto):
-    global ContadorProduto
-    NovoProduto = Produto(id=ContadorProduto, **produto.model_dump())
-    Produtos.append(NovoProduto)
-    ContadorProduto += 1
-    return NovoProduto
+def criar_produto(produto: CriarProduto):
+    global contador_produto
+    novo_produto = Produto(id=contador_produto, **produto.model_dump())
+    produtos.append(novo_produto)
+    contador_produto += 1
+    return novo_produto
 
 
 # Rota para listar todos os produtos
-
 @app.get("/produtos/", response_model=List[Produto])
-def listarprodutos():
-    return Produtos
+def listar_produtos():
+    return produtos
+
 
 # Rota para simular a criação do histórico de compras de um usuário
-
 @app.post("/historico_compras/{usuario_id}")
-def adicionarhistoricocompras(usuario_id: int, compras: HistoricoCompras):
-    if usuario_id not in [usuario.id for usuario in Usuarios]:
+def adicionar_historico_compras(usuario_id: int, compras: HistoricoCompras):
+    if usuario_id not in [usuario.id for usuario in usuarios]:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
-    HistoricoDeCompras[usuario_id] = compras.produtos_ids
+    historico_de_compras[usuario_id] = compras.produtos_ids
     return {"mensagem": "Histórico de compras atualizado"}
 
-# Rota para recomendações de produtos
 
+# Rota para recomendações de produtos
 @app.post("/recomendacoes/{usuario_id}", response_model=List[Produto])
-def recomendarprodutos(usuario_id: int, preferencias: Preferencias):
-    if usuario_id not in HistoricoDeCompras:
-        raise HTTPException(status_code=404, detail="Histórico de compras não encontrado")
+def recomendar_produtos(usuario_id: int, preferencias: Preferencias):
+    if usuario_id not in historico_de_compras:
+        raise HTTPException(
+            status_code=404, detail="Histórico de compras não encontrado"
+        )
 
     produtos_recomendados = []
 
     # Buscar produtos com base no histórico de compras do usuário
-
-    produtos_recomendados = [produto for produto_id in HistoricoDeCompras[usuario_id] for produto in Produtos if produto.id == produto_id]
+    produtos_recomendados = [
+        produto
+        for produto_id in historico_de_compras[usuario_id]
+        for produto in produtos
+        if produto.id == produto_id
+    ]
 
     # Filtrar as recomendações com base nas preferências
-    produtos_recomendados = [p for p in produtos_recomendados if p.categoria in preferencias.categorias] # Preferencias de categorias
-    produtos_recomendados = [p for p in produtos_recomendados if any(tag in preferencias.tags for tag in p.tags)] # Preferencias de tags
+    produtos_recomendados = [
+        p for p in produtos_recomendados if p.categoria in preferencias.categorias
+    ]  # Preferencias de categorias
+    produtos_recomendados = [
+        p
+        for p in produtos_recomendados
+        if any(tag in preferencias.tags for tag in p.tags)
+    ]  # Preferencias de tags
 
     return produtos_recomendados
 
-# Rota para cadastrar usuários
 
+# Rota para cadastrar usuários
 @app.post("/usuarios/", response_model=Usuario)
-def criarusuario(nome: str):
-    global ContadorUsuario
-    NovoUsuario = Usuario(id=ContadorUsuario, nome=nome)
-    Usuarios.append(NovoUsuario)
-    ContadorUsuario += 1
-    return NovoUsuario
+def criar_usuario(nome: str):
+    global contador_usuario
+    novo_usuario = Usuario(id=contador_usuario, nome=nome)
+    usuarios.append(novo_usuario)
+    contador_usuario += 1
+    return novo_usuario
+
 
 # Rota para listar usuários
-
 @app.get("/usuarios/", response_model=List[Usuario])
-def listarusuarios():
-    return Usuarios
+def listar_usuarios():
+    return usuarios
